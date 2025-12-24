6844ef465b5115fc7efe112b6900a32fc144bc56
apip <outan544@gmail.com>
2025-12-25 01:24:46 +0700

    Update TreeView.svelte

--- src/lib/components/app/TreeView.svelte
+++ src/lib/components/app/TreeView.svelte
@@ -1,16 +1,1 @@
-<script>
-import 'svelte/internal/disclose-version';
-import 'svelte/internal/flags/legacy';
-import 'svelte/internal/flags/async';
-import * as $ from 'svelte/internal/client';
 
-export default function App($$anchor) {
-	document.addEventListener('click', (e) => {
-		const folder = e.target.closest('li.folder');
-
-		if (!folder) return;
-
-		folder.classList.toggle('expanded');
-	});
-}
-</script>
