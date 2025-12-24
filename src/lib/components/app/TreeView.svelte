7bbacd3a7ce29ecf19182c94de506c3bc715ce3f
apip <outan544@gmail.com>
2025-12-24 21:15:55 +0700

    Update TreeView.svelte

--- src/lib/components/app/TreeView.svelte
+++ src/lib/components/app/TreeView.svelte
@@ -1,31 +1,11 @@
-<script>
-  function toggle(e) {
-    e.stopPropagation();
-    e.currentTarget.classList.toggle('expanded');
-  }
-</script>
-
-<ul class="tree-root">
-  <li class="folder" on:click={toggle}>
-    src
-    <ul>
-      <li class="folder" on:click={toggle}>
-        lib
-        <ul>
-          <li class="file">utils.ts</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-</ul>
+<div class="debug">
+TEKS INI HARUS HILANG!
+</div>
 
 <style>
-  ul {
-    list-style: none;
-    padding-left: 1rem;
+  .debug {
+    font-size: 14px;
+    color: #666;
+    margin-bottom: 8px;
   }
-  li.folder::before { content: "📁 "; }
-  li.file::before { content: "📄 "; }
-  li > ul { display: none; }
-  li.expanded > ul { display: block; }
 </style>
