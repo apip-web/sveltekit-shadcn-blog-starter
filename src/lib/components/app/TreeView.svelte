2e3da7e9b85ea9786247a9481f0bf2121086853e
apip <outan544@gmail.com>
2025-12-24 22:26:14 +0700

    Update TreeView.svelte

--- src/lib/components/app/TreeView.svelte
+++ src/lib/components/app/TreeView.svelte
@@ -1,11 +1,25 @@
-<div class="debug">
-TEKS INI HARUS HILANG! Part 2!
-</div>
+<script>
+  import TreeView from '$lib/components/app/TreeView.svelte';
 
-<style>
-  .debug {
-    font-size: 14px;
-    color: #666;
-    margin-bottom: 8px;
-  }
-</style>
+  const tree = {
+    label: 'src',
+    children: [
+      {
+        label: 'lib',
+        children: [
+          { label: 'utils.ts' }
+        ]
+      },
+      {
+        label: 'routes',
+        children: [
+          { label: '+page.svelte' }
+        ]
+      }
+    ]
+  };
+</script>
+
+<ul>
+  <TreeView {...tree} />
+</ul>
