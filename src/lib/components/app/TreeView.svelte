507270881c4b4f8a133621980e7aa76c7fd20e1c
apip <outan544@gmail.com>
2025-12-24 22:28:29 +0700

    Update TreeView.svelte

--- src/lib/components/app/TreeView.svelte
+++ src/lib/components/app/TreeView.svelte
@@ -1,25 +1,43 @@
 <script>
-  import TreeView from '$lib/components/app/TreeView.svelte';
+  export let label = '';
+  export let children = [];
+  let open = false;
 
-  const tree = {
-    label: 'src',
-    children: [
-      {
-        label: 'lib',
-        children: [
-          { label: 'utils.ts' }
-        ]
-      },
-      {
-        label: 'routes',
-        children: [
-          { label: '+page.svelte' }
-        ]
-      }
-    ]
-  };
+  function toggle() {
+    open = !open;
+  }
 </script>
 
-<ul>
-  <TreeView {...tree} />
-</ul>
+<li>
+  {#if children.length}
+    <div class="folder" on:click={toggle}>
+      {open ? '📂' : '📁'} {label}
+    </div>
+
+    {#if open}
+      <ul>
+        {#each children as child}
+          <TreeView {...child} />
+        {/each}
+      </ul>
+    {/if}
+  {:else}
+    <div class="file">📄 {label}</div>
+  {/if}
+</li>
+
+<style>
+  ul {
+    padding-left: 18px;
+  }
+
+  .folder {
+    cursor: pointer;
+    user-select: none;
+    font-weight: 600;
+  }
+
+  .file {
+    color: #555;
+  }
+</style>
