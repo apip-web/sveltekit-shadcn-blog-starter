f716c07220c1ec0608d1fc6abeba5ea507635b14
apip <outan544@gmail.com>
2025-12-24 23:34:43 +0700

    Update TreeView.svelte

--- src/lib/components/app/TreeView.svelte
+++ src/lib/components/app/TreeView.svelte
@@ -1,63 +1,12 @@
 <script>
-  let open = new Set();
+  import { onMount } from 'svelte';
 
-  function toggle(path) {
-    if (open.has(path)) {
-      open.delete(path);
-    } else {
-      open.add(path);
-    }
-    open = open; // trigger reactivity
-  }
+  onMount(() => {
+    document.querySelectorAll('li.folder').forEach((li) => {
+      li.addEventListener('click', (e) => {
+        e.stopPropagation();
+        li.classList.toggle('expanded');
+      });
+    });
+  });
 </script>
-
-<ul class="tree">
-  <li class="folder" class:expanded={open.has('src')} on:click={() => toggle('src')}>
-    src
-    <ul>
-      <li class="folder" class:expanded={open.has('src/lib')} on:click|stopPropagation={() => toggle('src/lib')}>
-        lib
-        <ul>
-          <li class="file">utils.ts</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-</ul>
-
-<style>
-  ul {
-    list-style: none;
-    padding-left: 1rem;
-  }
-
-  li {
-    margin: 4px 0;
-  }
-
-  li.folder {
-    cursor: pointer;
-    font-weight: 600;
-  }
-
-  li.file {
-    color: #555;
-    cursor: default;
-  }
-
-  li > ul {
-    display: none;
-  }
-
-  li.expanded > ul {
-    display: block;
-  }
-
-  li.folder::before {
-    content: "📁 ";
-  }
-
-  li.file::before {
-    content: "📄 ";
-  }
-</style>
